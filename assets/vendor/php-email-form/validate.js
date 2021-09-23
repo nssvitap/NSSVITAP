3cde9c1a5aa24ad254231ff1f19e1867c040a635
Praisepaul <praise20024christ@gmail.com>
2021-09-23 17:18:52 +0530

    Final Updates

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,20 +1,34 @@
-var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
+window.addEventListener('DOMContentLoaded',function () {
+	var form = document.getElementById("my-form");
+	var status = document.getElementById("status");
+
+	function success() {
+		form.reset();
+		status.classList.add('success');
+		status.innerHTML = "Thank You! Your form is submitted.";
+	}
+	function error(){
+		status.innerHTML = "Oops! We encounterred an issue.";
+		status.classList.add('error');
+	}
+	form.addEventListener("submit", function (ev) {
+		ev.preventDefault();
+		var data = new FormData(form);
+		ajax(form.method, form.action, data , success, error);
+	});
+});
+
+function ajax(method, url, data, success, error) {
+var xhr = new XMLHttpRequest();
+xhr.open(method, url);
+xhr.setRequestHeader("Accept", "application/json");
+xhr.onreadystatechange = function() {
+	if(xhr.readyState !== XMLHttpRequest.DONE) return;
+	if(xhr.status === 200) {
+		success(xhr.response, xhr.responseType);
+	}else{
+		error(xhr.status, xhr.response, xhr.responseText);
+	}
+};
+xhr.send(data);
+}
