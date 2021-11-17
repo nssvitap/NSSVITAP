6b5f7b612a7d64c2108b43eb90e295121db7ee06
Praisepaul <praise20024christ@gmail.com>
2021-11-17 11:11:53 +0530

    Added loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 (function() {
   "use strict";
 
@@ -248,3 +249,9 @@
   });
 
 })()
+const progressBar = document.getElementsByClassName('progress-bar')[0]
+setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar)
+  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+  progressBar.style.setProperty('--width', width + .1)
+}, 5)
